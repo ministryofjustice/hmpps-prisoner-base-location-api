74466ba2a04e064cd95dc93d91c4340e761049d5
Chris Waugh <238270+chriswaugh@users.noreply.github.com>
2025-09-05 15:51:32 +0100

    ESWE-1410: Fixed some compiler warnings

--- src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerbaselocationapi/models/hmpps/PrisonerBaseLocation.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/prisonerbaselocationapi/models/hmpps/PrisonerBaseLocation.kt
@@ -6,20 +6,20 @@ import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
 
 data class PrisonerBaseLocation(
-  @Schema(description = "In prison or not")
-  @JsonInclude(JsonInclude.Include.NON_NULL)
+  @param:Schema(description = "In prison or not")
+  @param:JsonInclude(JsonInclude.Include.NON_NULL)
   val inPrison: Boolean? = null,
-  @Schema(description = "Prison ID", example = "MDI")
-  @JsonInclude(JsonInclude.Include.NON_NULL)
+  @param:Schema(description = "Prison ID", example = "MDI")
+  @param:JsonInclude(JsonInclude.Include.NON_NULL)
   val prisonId: String? = null,
-  @Schema(description = "The last prison for the prisoner (which is the same as the prisonId if they are still inside prison)", example = "MDI")
-  @JsonInclude(JsonInclude.Include.NON_NULL)
+  @param:Schema(description = "The last prison for the prisoner (which is the same as the prisonId if they are still inside prison)", example = "MDI")
+  @param:JsonInclude(JsonInclude.Include.NON_NULL)
   val lastPrisonId: String? = null,
-  @Schema(description = "Last movement type", example = "Admission")
-  @JsonInclude(JsonInclude.Include.NON_NULL)
+  @param:Schema(description = "Last movement type", example = "Admission")
+  @param:JsonInclude(JsonInclude.Include.NON_NULL)
   val lastMovementType: LastMovementType? = null,
-  @Schema(description = "Reception date", example = "2025-04-01")
-  @JsonInclude(JsonInclude.Include.NON_NULL)
+  @param:Schema(description = "Reception date", example = "2025-04-01")
+  @param:JsonInclude(JsonInclude.Include.NON_NULL)
   val receptionDate: LocalDate? = null,
 )
 
