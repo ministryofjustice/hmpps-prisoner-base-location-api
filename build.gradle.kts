ff3d19068b66eddea4e137e192b1e58d93d482a9
rickchoijd <rick.choi@justice.gov.uk>
2026-02-03 18:17:24 +0000

    ESWE-1671: Spring Boot 4 upgrade
    Version upgrade:
    - plugin `uk.gov.justice.hmpps.gradle-spring-boot`: `10.0.3`
    - Kotlin: `2.3.0`
    - dependencies:
    - `hmpps-kotlin-spring-boot-starter`,
    `hmpps-kotlin-spring-boot-starter-test`: `2.0.0`
    - unpin Netty version
    
    Add dependencies (Spring starters/modules)
    - `spring-boot-starter-webclient`
    
    Add test dependencies (Spring starters/modules)
    - `spring-boot-webtestclient`
    
    and more:
    - `springdoc-openapi-starter-webmvc-ui` : `3.0.1`
    - `swagger-parser-v3` : `2.1.37`
    - `wiremock-standalone` : `3.13.2`
    
    Fixes during upgrade
    - fixes for Nullability changes
    - fixes for upgrading Jackson 3
    - fix warnings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,22 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "9.3.0"
-  kotlin("plugin.spring") version "2.2.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.3"
+  kotlin("plugin.spring") version "2.3.0"
 }
 
 configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
-ext["netty.version"] = "4.1.130.Final"
-
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.8.1") {
-    implementation("org.apache.commons:commons-lang3:3.18.0")
-  }
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.0.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
-  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.13")
+  implementation("org.springframework.boot:spring-boot-starter-webclient")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.1")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:1.8.1")
-  testImplementation("org.wiremock:wiremock-standalone:3.13.1")
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.31") {
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.0.0")
+  testImplementation("org.springframework.boot:spring-boot-webtestclient")
+  testImplementation("org.wiremock:wiremock-standalone:3.13.2")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.37") {
     exclude(group = "io.swagger.core.v3")
   }
 }
