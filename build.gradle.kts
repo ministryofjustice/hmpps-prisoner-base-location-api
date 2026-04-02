fa923295933cba6d205299c9e0160dcc144bd491
rickchoijd <rick.choi@justice.gov.uk>
2026-04-02 16:14:42 +0100

    ESWE-1809: Update dependencies, remove Trivy
    Update plugin/dependencies
    - HMPPS Gradle Spring Boot plugin to `10.1.2`
    - Kotlin to `2.3.20`
    - Spring Boot to `4.0.5`
    - HMPPS Kotlin lib to `2.1.0`
    - Gradle wrapper to `9.4.1`
    
    and more:
    
    Update Helm chart
    - `generic-prometheus-alerts` to `1.15`
    
    Remove Trivy scan

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.5"
-  kotlin("plugin.spring") version "2.3.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.1.2"
+  kotlin("plugin.spring") version "2.3.20"
 }
 
 configurations {
@@ -8,12 +8,12 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.0.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.1.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-webclient")
   implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
 
-  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.0.2")
+  testImplementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter-test:2.1.0")
   testImplementation("org.springframework.boot:spring-boot-webtestclient")
   testImplementation("org.wiremock:wiremock-standalone:3.13.2")
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.39") {
