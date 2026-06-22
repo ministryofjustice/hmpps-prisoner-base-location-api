7dab3526abbb7a94575ecf33c2d60cfd3aaf1cda
rickchoijd <rick.choi@justice.gov.uk>
2026-06-22 16:14:15 +0100

    Patching demo: Pin Netty
    pin Netty to `4.2.15.Final`

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,8 @@ configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
+ext["netty.version"] = "4.2.15.Final"
+
 dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.2.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
